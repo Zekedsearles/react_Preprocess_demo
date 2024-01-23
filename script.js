042faa5dd1bdff2854fb1f7a71d38bb6f9ec6be1
Ezekiel Searles <zekedsearles@gmail.com>
2024-01-22 16:22:32 -0800

    hello world

--- script.js
+++ script.js
@@ -3,58 +3,10 @@ var App = function App() {
         Template,
         null,
         React.createElement(
-            "h1",
+            'h1',
             null,
-            "Main content"
+            'Main content'
         )
-    );
-};
-var Template = function Template(props) {
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(Navbar, null),
-        React.createElement(
-            "div",
-            { className: "container py-4" },
-            React.createElement(
-                "div",
-                { className: "row" },
-                React.createElement(
-                    "div",
-                    { className: "col-12 col-md-9" },
-                    props.children
-                ),
-                React.createElement(Sidebar, null)
-            )
-        ),
-        React.createElement(Footer, null)
-    );
-};
-
-var Navbar = function Navbar() {
-    return React.createElement(
-        "nav",
-        { className: "navbar navbar-expand-lg navbar-light bg-light" },
-        React.createElement("a", { className: "navbar-brand", href: "#" })
-    );
-};
-var Sidebar = function Sidebar() {
-    return React.createElement(
-        "div",
-        { className: "d-none d-md-block col-md-3" },
-        React.createElement(
-            "div",
-            { className: "boarder boarder-primary py-4 px-3" },
-            "Sidebar"
-        )
-    );
-};
-var Footer = function Footer() {
-    return React.createElement(
-        "div",
-        { className: "boarder-top p-2" },
-        "Template Demo"
     );
 };
 
--- src/script.js
+++ src/script.js
@@ -5,47 +5,6 @@ const App = () => {
         </Template>
     );
 }
-const Template = (props) => {
-    return (
-        <div>
-            <Navbar />
-            <div className="container py-4">
-                <div className="row">
-                    <div className="col-12 col-md-9">
-                        {props.children}
-                    </div>
-                    <Sidebar />
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
-}
-
-
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"></a>
-        </nav>
-    );
-}
-const Sidebar = () => {
-    return (
-        <div className="d-none d-md-block col-md-3">
-            <div className="boarder boarder-primary py-4 px-3">
-                Sidebar
-            </div>
-        </div>
-    );
-}
-const Footer = () => {
-    return (
-        <div className="boarder-top p-2">
-            Template Demo
-        </div>
-    );
-}
   
   const container = document.getElementById('root');
   const root = ReactDOM.createRoot(container);
